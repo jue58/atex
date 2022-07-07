a8ac2732e8057786bc2354f4f8113b2f005a74b5
Rikiya Kawakami <ricky.k.yang@gmail.com>
2022-07-08 02:11:56 +0900

    Import custom star list from csv

--- message_bot/app/src/main.rb
+++ message_bot/app/src/main.rb
@@ -12,41 +12,38 @@ def format_name(p)
   p['encounter'] ? "#{p['name']} [#{p['encounter']}]" : p['name']
 end
 
-def send_roster(roster, bot)
+def create_roster_text(roster)
   teams = roster.group_by{|v| v['team_id']}
   name_list = teams.map do |_, t|
     t.map {|p| format_name(p)}
   end
+  return '' if name_list.empty?
 
-  unless name_list.empty?
-    roster_str = name_list.map{|v| "- #{v.join(",  ")}\n"}.join
-    message = <<EOS
-----------------------------------------------------
-今回の参加者
+  roster_str = name_list.map{|v| "- #{v.join(",  ")}\n"}.join
+  <<~EOS
+    ----------------------------------------------------
+    今回の参加者
 
-#{roster_str}
-EOS
-    bot.send_message(message)
-  end
+    #{roster_str}
+  EOS
 end
 
-def send_stars(db, roster, bot)
+def create_stars_text(db, roster)
   name_list = roster.map{|v| v['name']}
   matched_stars = db.match_stars(name_list)
 
   p "matched_stars: #{matched_stars}"    
 
   star_result = name_list.zip(matched_stars).to_h.compact
-  unless star_result.empty?
-    result_str = star_result.map{|k, v| "#{k} => #{v}\n"}.join
-    message2 = <<EOS
-----------------------------------------------------
-もしかしたらこの人とマッチしてるかも...？
+  return '' if star_result.empty?
 
-#{result_str}
-EOS
-    bot.send_message(message2)
-  end
+  result_str = star_result.map{|k, v| "#{k} => #{v}\n"}.join
+  <<~EOS
+    ----------------------------------------------------
+    もしかしたらこの人とマッチしてるかも...？
+
+    #{result_str}
+  EOS
 end
 
 #################################################
@@ -60,11 +57,14 @@ get_roster(db) do |roster|
   p "roster: #{roster}"
 
   unless roster.empty?
-    roster.map do |v|
+    roster.each do |v|
       num = db.increment_encounter(v['name'])
       v['encounter'] = num if num > 1
     end
-    send_roster(roster, bot)
-    send_stars(db, roster, bot)
+
+    roster_text = create_roster_text(roster)
+    bot.send_message(roster_text) unless roster_text.empty?
+    stars_text = create_stars_text(db, roster)
+    bot.send_message(stars_text) unless stars_text.empty?
   end
 end
